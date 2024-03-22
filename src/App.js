28fb01b4768a9ddf769a5ab160a869aa03edd4f3
Ran Jan <ranjan@Rans-MacBook-Air.local>
2024-03-22 15:10:43 -0600

    added login functionality

--- src/App.js
+++ src/App.js
@@ -1,36 +1,48 @@
 import logo from './logo.svg';
 import './App.css';
+import React from "react";
 
+//material ui
+import Paper from '@mui/material/Paper';
+import Chip from '@mui/material/Chip';
+import FaceIcon from '@mui/icons-material/Face';
+import LockIcon from '@mui/icons-material/Lock';
+import Signup from './signup';
+import Login from './login';
+import Switch from '@mui/material/Switch';
 function App() {
+
+  const [checked, setChecked] = React.useState(true);
+
+  const handleChange = (event) => {
+    setChecked(event.target.checked);
+  };
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-          hello react,Demo content
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-      <form>
-  <label>
-    Name:
-    <input type="text" name="name" />
-  </label>
-  <label>
-    subject:
-    <input type="text" name="subject" />
-  </label>
-  <input type="submit" value="Submit" />
-</form>
+     <Paper elevation={3}  style ={{padding:"10px"}}>
+
+      {checked ? (
+     <Chip icon={<FaceIcon />} label="sign up" variant="outlined" />
+      ) : (
+    
+    <Chip icon={<LockIcon />} label="login" variant="outlined" />
+      )}
+      
+
+    <Switch
+checked={checked}
+onChange={handleChange}
+inputProps={{ 'aria-label': 'controlled' }}
+/>
+
+{checked ? <Signup /> : <Login />}
+</Paper>
     </div>
+
+ 
+
   );
 }
 
